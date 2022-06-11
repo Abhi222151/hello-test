e95411494e8cc7c897e9267854007f7503bc336c
Abhijeet <abhijeetgupta07@gmail.com>
2022-06-11 22:50:58 +0530

    update java file

--- Test.java
+++ Test.java
@@ -5,6 +5,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+//Abhijeet Gupta creation
 //Pojo Class
 @Entity
 public class User {
