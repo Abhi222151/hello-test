da2116f3ab59b43c95fc0615647d6ea2b570e2a3
abhi222151 <abhijeetgupta07@gmail.com>
2022-06-12 23:26:33 +0530

    updated the .java file

--- Test.java
+++ Test.java
@@ -4,6 +4,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+//  This project is just to learn git and their commands
+
 
 //Abhijeet Gupta creation
 //Pojo Class
